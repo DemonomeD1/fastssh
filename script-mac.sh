8366f49600e6d576392307c7d02b6958c7039736
DemonomeD1 <itimaro@mail.ru>
2022-02-26 23:26:37 +0300

    Fixed unaccepted license agreement

--- script-mac.sh
+++ script-mac.sh
@@ -8,7 +8,7 @@ ngrok tcp 3389 -log=stdout > ngrok.log &
 
 # install VirtualBox extpack 
 wget https://download.virtualbox.org/virtualbox/6.1.32/Oracle_VM_VirtualBox_Extension_Pack-6.1.32.vbox-extpack
-sudo VBoxManage extpack install Oracle_VM_VirtualBox_Extension_Pack-6.1.32.vbox-extpack
+sudo VBoxManage extpack install Oracle_VM_VirtualBox_Extension_Pack-6.1.32.vbox-extpack --accept-license=715c7246dc0f779ceab39446812362b2f9bf64a55ed5d3a905f053cfab36da9e
 
 # create and start vm with vrdp
 sudo VBoxManage createvm --name testArch --ostype ArchLinux_64 --register
