29febaa660ebddf9ac06dd5a2a417887083e7fd9
DemonomeD1 <itimaro@mail.ru>
2022-02-26 22:26:24 +0300

    Added ngrokk setup in action and vm start

--- script-mac.sh
+++ script-mac.sh
@@ -1,6 +1,9 @@
 # disable spotlight indexing
 sudo mdutil -i off -a
 
+wget https://download.virtualbox.org/virtualbox/6.1.32/Oracle_VM_VirtualBox_Extension_Pack-6.1.32.vbox-extpack
+sudo VBoxManage extpack install Oracle_VM_VirtualBox_Extension_Pack-6.1.32.vbox-extpack
+
 sudo VBoxManage createvm --name testArch --ostype ArchLinux_64 --register
 sudo VBoxManage modifyvm testArch --cpus 1 --memory 1024 --vram 12
 sudo VBoxManage createhd --filename ./testArch.vdi --size 5120 --variant Standard
@@ -10,4 +13,4 @@ sudo VBoxManage storagectl testArch --name "IDE Controller" --add ide
 wget http://mirror.rackspace.com/archlinux/iso/2022.02.01/archlinux-2022.02.01-x86_64.iso
 sudo VBoxManage storageattach testArch --storagectl "IDE Controller" --port 0  --device 0 --type dvddrive --medium ./archlinux-2022.02.01-x86_64.iso
 sudo VBoxManage modifyvm testArch --vrde on
-# VBoxHeadless --startvm testArch
+sudo vboxmanage startvm testArch --type headless
