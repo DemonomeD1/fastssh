a1b770894d7370472635f28fd36a03fc17532a4a
dm1sh <me@dmitriy.icu>
2022-02-26 20:47:08 +0300

    Added basic virtualbox vm setup

--- script-mac.sh
+++ script-mac.sh
@@ -1,3 +1,13 @@
 # disable spotlight indexing
 sudo mdutil -i off -a
-echo Edit script-mac.sh in your fastmac repo to auto-run commands in your Mac instances
+
+sudo VBoxManage createvm --name testArch --ostype ArchLinux_64 --register
+sudo VBoxManage modifyvm testArch --cpus 1 --memory 1024 --vram 12
+sudo VBoxManage createhd --filename ./testArch.vdi --size 5120 --variant Standard
+sudo VBoxManage storagectl testArch --name "SATA Controller" --add sata --bootable on
+sudo VBoxManage storageattach testArch --storagectl "SATA Controller" --port 0 --device 0 --type hdd --medium ./testArch.vdi
+sudo VBoxManage storagectl testArch --name "IDE Controller" --add ide
+wget http://mirror.rackspace.com/archlinux/iso/2022.02.01/archlinux-2022.02.01-x86_64.iso
+sudo VBoxManage storageattach testArch --storagectl "IDE Controller" --port 0  --device 0 --type dvddrive --medium ./archlinux-2022.02.01-x86_64.iso
+sudo VBoxManage modifyvm testArch --vrde on
+# VBoxHeadless --startvm testArch
